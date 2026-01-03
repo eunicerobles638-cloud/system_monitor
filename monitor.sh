d19b886d940a420da12111d5ba4ff806d2659d6b
Eunice_Robles <eunicerobles638gmail.com>
2026-01-04 02:15:25 +0800

    Cleaned up and finalized English Interface

--- monitor.sh
+++ monitor.sh
@@ -1,1 +1,6 @@
-echo "Final monitor update"
+#!/bin/bash
+echo "Welcome to System Monitor v2"
+echo -n "Please enter your name: "
+read name
+echo "Thank you, $name! We are now monitoring your system."
+
